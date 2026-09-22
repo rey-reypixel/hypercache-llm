f258e58989dbe2a4cd35269b24aa2382dc435764
Mahashreyaa Pathak <mahashreyaa@gmail.com>
2026-09-22 20:06:07 +0530

    Add CMake toolchain file for CMake 4.x compatibility; use in all CI configure steps

--- cmake/policy.cmake
+++ cmake/policy.cmake
@@ -1,2 +1,4 @@
-# CMake toolchain for CMake 4.x compatibility
+# CMake toolchain for CMake 4.x compatibility with old FetchContent dependencies
+# This sets the policy version before any cmake_minimum_required() calls
+
 cmake_policy(VERSION 3.5...4.0)
